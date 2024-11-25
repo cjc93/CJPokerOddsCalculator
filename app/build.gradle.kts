0a990f87ffc4a48247cde3db1ab1f3624787a58d
cjc93 <68903433+cjc93@users.noreply.github.com>
2024-11-25 16:30:42 +0000

    bump version to 2.16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.leslie.cjpokeroddscalculator"
         minSdk = 26
         targetSdk = 34
-        versionCode = 29
-        versionName = "2.15"
+        versionCode = 30
+        versionName = "2.16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
