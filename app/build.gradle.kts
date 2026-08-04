412223b2c14c085d2cdf85a96a84b483a3b83564
cjc93 <68903433+cjc93@users.noreply.github.com>
2026-08-04 18:22:12 +0100

    Bump versionCode to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.leslie.cjpokeroddscalculator"
         minSdk = 34
         targetSdk = 37
-        versionCode = 36
+        versionCode = 37
         versionName = "2.21"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
