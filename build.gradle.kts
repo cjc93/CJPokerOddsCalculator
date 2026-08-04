4f7d78cf5d96e28cec3ac927778e4450be754ed2
cjc93 <68903433+cjc93@users.noreply.github.com>
2026-08-04 11:02:31 +0100

    Setup Firebase Crashlytics

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "9.3.1" apply false
+    alias(libs.plugins.android.application) apply false
+
+    // Firebase Plugins
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.crashlytics) apply false
 }
